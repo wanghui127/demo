c5e4c205a991304f9ce7c5cd743f5546f11ed943
wanghui <123456>
2019-07-12 14:08:37 +0800

    ico设置

--- src/main/java/com/example/demo/controller/joke/HelloController.java
+++ src/main/java/com/example/demo/controller/joke/HelloController.java
@@ -6,10 +6,7 @@ import com.example.demo.utils.JsonResult;
 import com.example.demo.utils.JsonResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -45,6 +42,12 @@ public class HelloController extends JsonResultUtil{
     @RequestMapping("/gundong")
     public String gundong(){
         return "/gundong";
+    }
+
+    //postman测试（可删）
+    @PostMapping("entityTest")
+    public void entityTest(@RequestBody Joke joke){
+        joke.getContent();
     }
 
 
